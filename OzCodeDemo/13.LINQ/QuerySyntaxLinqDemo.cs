ffb5540ca3501b57bf1f401fd6fb9533ff6164c2
Jeff Yates <jeff.yates@alumni.manchester.ac.uk>
2016-10-28 03:21:18 -0500

    Did some shenanigans with joins

--- OzCodeDemo/13.LINQ/QuerySyntaxLinqDemo.cs
+++ OzCodeDemo/13.LINQ/QuerySyntaxLinqDemo.cs
@@ -13,15 +13,36 @@ namespace OzCodeDemo._13.LINQ
 		{
 			Debugger.Break();
 
-			var states =
-				from customer in CustomersRepository.LoadCustomersFromDb()
-				let address = customer.Address
-				where !string.IsNullOrWhiteSpace(address.State)
-				group address by address.State
-				into stateGroup
-				select stateGroup.Key;
+			var customers = CustomersRepository.LoadCustomersFromDb();
 
-			Debug.Assert( states.Count() == 19 );
+			var customersEligible = customers.Where(c => c.Address.State != null).ToList();
+
+			var salesReps = new[]
+			{
+				new {Name = "Bob", States = new [] {"PA", "AK", "WI", "IN", "NM", "NJ" }, Address = "Bob's House"},
+				new {Name = "Danny", States = new [] {"MI", "HI", "NY", "TX", "AL","MD"}, Address = "Bob's House"},
+				new {Name = "Albert", States = new [] {"KS", "UT", "DE", "LA" }, Address = "Not Bob's House"},
+				new {Name = "Samantha", States = new [] { "MN", "OH", "IL", "CA", "ND", "SD", "VA", "SC"}, Address = "Bob's House"},
+			};
+
+			var customersWithSalesRep1 =
+				from rep in salesReps
+				from state in rep.States
+				join customer in customersEligible on state equals customer.Address.State
+				select new { Customer = customer, SalesRep = rep };
+
+			Debug.Assert( customersWithSalesRep1.Count() == customersEligible.Count, "Every customer should have a sales rep" );
+
+
+			var customersWithSalesRep2 =
+				from rep in salesReps
+				from state in rep.States
+				join customer in customersEligible on state equals customer.Address.State into customersWithRep
+				from customerWithRep in customersWithRep.DefaultIfEmpty()
+				where customerWithRep != null
+				select new {Customer = customerWithRep, SalesRep = rep};
+
+			Debug.Assert( customersWithSalesRep2.Count() == customersEligible.Count, "Every customer should have a sales rep");
 		}
 	}
 }
